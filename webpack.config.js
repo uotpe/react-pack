aa8c7fd423bf26737cd3cfc3fce452c4794d55cc
Jason <zhoujj519@gmail.com>
2016-10-27 21:24:42 -0400

    add web pack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./entry.js",
+  output: {
+    path : __dirname,
+    filename : "bundle.js"
+  },
+  module:{
+    loaders : [
+      {
+        test : /\.css$/,
+        loader : 'style!css'
+      },
+      {
+        test : /\.(js|jsx)$/,
+        loader : 'babel',
+        query : {
+          presets : ['es2015','stage-0','react'],
+          plugins : ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
